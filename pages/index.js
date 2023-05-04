881fc4aa5674ec55bbd4e07d854e2fa1da94e338
innocentdavid <paulinnocent04@gmail.com>
2023-05-04 16:27:19 +0100

    d

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
-
-
-
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Inputfeild from '../components/inputfeild'
 import Emailverifybutton from '../components/Buttons/Emailverifybutton'
 import Data from '../components/Data'
-// import Table from '../components/table'
+// import Table from '../components/Table'
 
 
 export default function Home() {
-  return (
 
+  return (
     <div className="mx-auto w-[80%] my-[50px] border border-slate-500 rounded-lg ">
       <header className="flex justify-between align-center m-5 " >
-      <div className="w-[50%]">
-        <Inputfeild
-            type= 'search'
-            name = 'search'
-            plholder= 'Type here'
+        <div className="w-[50%]">
+          <Inputfeild
+            type='search'
+            name='search'
+            plholder='Type here'
           />
-      </div>
+        </div>
 
         <Emailverifybutton
           title='Add data'
@@ -30,8 +27,8 @@ export default function Home() {
         />
       </header>
 
-      <Data/>
-      {/* <Table/> */}
+      <Data />
+      {/* <Table /> */}
 
     </div>
 
