44d9ff37cf02411b497d00fd5ca5231e9487b796
innocentdavid <paulinnocent04@gmail.com>
2023-05-13 00:27:33 -0700

    f

--- components/table.js
+++ components/table.js
@@ -1,19 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import MaterialReactTable from 'material-react-table';
-import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    IconButton,
-    MenuItem,
-    Stack,
-    TextField,
-    Tooltip,
-} from '@mui/material';
-import { Delete, Edit } from '@mui/icons-material';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 // import LoadingModal from './LoadingModal ';
@@ -343,65 +329,65 @@ const Table = ({ data, page, applicationsLoading }) => {
                     </div>
                 )}
             />
-            <CreateNewAccountModal
+            {/* <CreateNewAccountModal
                 columns={columns}
                 open={createModalOpen}
                 onClose={() => setCreateModalOpen(false)}
                 onSubmit={handleCreateNewRow}
-            />
+            /> */}
         </div>
     );
 };
 
 //Table of creating a mui dialog modal for creating new rows
-export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
-    const [values, setValues] = useState(() =>
-        columns.reduce((acc, column) => {
-            acc[column.accessorKey ?? ''] = '';
-            return acc;
-        }, {}),
-    );
+// export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
+//     const [values, setValues] = useState(() =>
+//         columns.reduce((acc, column) => {
+//             acc[column.accessorKey ?? ''] = '';
+//             return acc;
+//         }, {}),
+//     );
 
-    const handleSubmit = () => {
-        //put your validation logic here
-        onSubmit(values);
-        onClose();
-    };
+//     const handleSubmit = () => {
+//         //put your validation logic here
+//         onSubmit(values);
+//         onClose();
+//     };
 
-    return (
-        <Dialog open={open}>
-            <DialogTitle textAlign="center">Create New Account</DialogTitle>
-            <DialogContent>
-                <form onSubmit={(e) => e.preventDefault()}>
-                    <Stack
-                        sx={{
-                            width: '100%',
-                            minWidth: { xs: '300px', sm: '360px', md: '400px' },
-                            gap: '1.5rem',
-                        }}
-                    >
-                        {columns?.map((column) => (
-                            <TextField
-                                key={column.accessorKey}
-                                label={column.header}
-                                name={column.accessorKey}
-                                onChange={(e) =>
-                                    setValues({ ...values, [e.target.name]: e.target.value })
-                                }
-                            />
-                        ))}
-                    </Stack>
-                </form>
-            </DialogContent>
-            <DialogActions sx={{ p: '1.25rem' }}>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button color="secondary" onClick={handleSubmit} variant="contained">
-                    Create New Account
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
-};
+//     return (
+//         <Dialog open={open}>
+//             <DialogTitle textAlign="center">Create New Account</DialogTitle>
+//             <DialogContent>
+//                 <form onSubmit={(e) => e.preventDefault()}>
+//                     <Stack
+//                         sx={{
+//                             width: '100%',
+//                             minWidth: { xs: '300px', sm: '360px', md: '400px' },
+//                             gap: '1.5rem',
+//                         }}
+//                     >
+//                         {columns?.map((column) => (
+//                             <TextField
+//                                 key={column.accessorKey}
+//                                 label={column.header}
+//                                 name={column.accessorKey}
+//                                 onChange={(e) =>
+//                                     setValues({ ...values, [e.target.name]: e.target.value })
+//                                 }
+//                             />
+//                         ))}
+//                     </Stack>
+//                 </form>
+//             </DialogContent>
+//             <DialogActions sx={{ p: '1.25rem' }}>
+//                 <Button onClick={onClose}>Cancel</Button>
+//                 <Button color="secondary" onClick={handleSubmit} variant="contained">
+//                     Create New Account
+//                 </Button>
+//             </DialogActions>
+//         </Dialog>
+//     );
+// };
 
 const validateRequired = (value) => !!value.length;
 const validateEmail = (email) =>
