a4de5af03b851f4a1d0ea7db270bee426d68b9ee
innocentdavid <paulinnocent04@gmail.com>
2023-05-13 00:44:21 -0700

    gggg

--- components/table.js
+++ components/table.js
@@ -1,14 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import MaterialReactTable from 'material-react-table';
-import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    Stack,
-    TextField,
-} from '@mui/material';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import LoadingModal from './LoadingModal ';
@@ -167,74 +158,8 @@ const Table = ({ data, page, applicationsLoading }) => {
                     </div>
                 )}
             />
-            {/* <CreateNewAccountModal
-                columns={columns}
-                open={createModalOpen}
-                onClose={() => setCreateModalOpen(false)}
-                onSubmit={handleCreateNewRow}
-            /> */}
         </div>
     );
 };
-
-//Table of creating a mui dialog modal for creating new rows
-// export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
-//     const [values, setValues] = useState(() =>
-//         columns.reduce((acc, column) => {
-//             acc[column.accessorKey ?? ''] = '';
-//             return acc;
-//         }, {}),
-//     );
-
-//     const handleSubmit = () => {
-//         //put your validation logic here
-//         onSubmit(values);
-//         onClose();
-//     };
-
-//     return (
-//         <Dialog open={open}>
-//             <DialogTitle textAlign="center">Create New Account</DialogTitle>
-//             <DialogContent>
-//                 <form onSubmit={(e) => e.preventDefault()}>
-//                     <Stack
-//                         sx={{
-//                             width: '100%',
-//                             minWidth: { xs: '300px', sm: '360px', md: '400px' },
-//                             gap: '1.5rem',
-//                         }}
-//                     >
-//                         {columns?.map((column) => (
-//                             <TextField
-//                                 key={column.accessorKey}
-//                                 label={column.header}
-//                                 name={column.accessorKey}
-//                                 onChange={(e) =>
-//                                     setValues({ ...values, [e.target.name]: e.target.value })
-//                                 }
-//                             />
-//                         ))}
-//                     </Stack>
-//                 </form>
-//             </DialogContent>
-//             <DialogActions sx={{ p: '1.25rem' }}>
-//                 <Button onClick={onClose}>Cancel</Button>
-//                 <Button color="secondary" onClick={handleSubmit} variant="contained">
-//                     Create New Account
-//                 </Button>
-//             </DialogActions>
-//         </Dialog>
-//     );
-// };
-
-const validateRequired = (value) => !!value.length;
-const validateEmail = (email) =>
-    !!email.length &&
-    email
-        .toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        );
-const validateAge = (age) => age >= 18 && age <= 50;
 
 export default Table;
