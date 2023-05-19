77109324dd5f4872d94314fc0d88ab9fa77e77ef
innocentdavid <paulinnocent04@gmail.com>
2023-05-19 07:26:21 +0100

    d

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,12 @@ import Table from '../components/table'
 import API from '../components/API';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { parseCookies } from 'nookies';
+// import { parseCookies } from 'nookies';
+import Cookies from 'cookies';
 
-export default function Home({ data }) {
-// export default function Home() {
-//   const data = []
+// export default function Home({ data }) {
+export default function Home() {
+  const data = []
   const [applications, setApplications] = useState(data)
 
   // get applications
@@ -39,46 +40,86 @@ export default function Home({ data }) {
 
 
 
-export async function getServerSideProps(context) {
-  const cookies = context.req.cookies;
-  // const jwtCookie = cookies.get('jwt');
-  // console.log(jwtCookie);
-  const Ncookies = parseCookies(context);
-  console.log("Ncookies: ");
-  console.log(Ncookies);
-  console.log("cookies: ");
-  console.log(cookies);
-  // console.log(cookies['jwt']);
+// export async function getServerSideProps(context) {
+//   const Ncookies = new Cookies(context.req);
+//   const cookieValue = Ncookies.get('jwt');
+//   console.log("cookieValue: ");
+//   console.log(cookieValue);
+  
+//   const cookies = context?.req?.cookies;
+//   console.log("cookies: ");
+//   console.log(cookies);
+//   // console.log(cookies['jwt']);
+  
+//   // const jwtCookie = cookies.get('jwt');
+//   // console.log(jwtCookie);
+//   // const Ncookies = parseCookies(context);
+//   // console.log("Ncookies: ");
+//   // console.log(Ncookies);
 
-  try {
-    var data = []
-    if (cookies['jwt']){
-      console.log('eysdkl');
-      // const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/applications/`, {
-      // headers: {
-      //   'Content-Type': 'application/json',
-      //   Authorization: `Bearer ${Ncookies['jwt']}`, // get JWT token from cookie
-      // },
-      // // withCredentials: true
-      // }).catch(err => {
-      //   console.log(err);
-      // });
+//   try {
+//     var data = []
+//     // if (cookies['jwt']){
+//       console.log('eysdkl');
+//       const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/applications/`, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//           Authorization: `Bearer ${cookieValue}`, // get JWT token from cookie
+//       },
+//       withCredentials: true
+//       }).catch(err => {
+//         console.log(err);
+//       });
       
-      // if(res?.data){
-      //   data = res.data
-      // }
-    }
-    return {
-      props: {
-        data
-      },
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        data: [],
-      },
-    };
-  }
-}
+//       if(res?.data){
+//         data = res.data
+//       }
+//     // }
+//     return {
+//       props: {
+//         data
+//       },
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     return {
+//       props: {
+//         data: [],
+//       },
+//     };
+//   }
+// }
+
+
+
+
+// export async function getServerSideProps(context) {
+//   const cookies = new Cookies(context.req);
+//   const cookieValue = cookies.get('jwt');
+
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/applications/`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${cookieValue}`, // get JWT token from cookie
+//       },
+//       credentials: 'include',
+//     });
+
+//     const data = await res.json();
+
+//     return {
+//       props: {
+//         data,
+//       },
+//     };
+//   } catch (error) {
+//     // console.log(error);
+//     return {
+//       props: {
+//         data: [],
+//       },
+//     };
+//   }
+// }
