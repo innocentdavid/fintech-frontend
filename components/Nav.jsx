f46d19a8a3a3f574e65113302db75011b7104112
innocentdavid <paulinnocent04@gmail.com>
2023-05-24 09:05:36 +0100

    updated

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -38,20 +38,24 @@ export default function Nav() {
             <div className={`${showMobileMenu ? 'opacity-100' : 'opacity-0 md:opacity-100 pointer-events-none md:pointer-events-auto'} z-10 flex flex-col md:flex-row md:items-center md:gap-2 absolute md:static top-full right-0 bg-slate-300 bg-opacity-25 backdrop-blur-lg backdrop-filter backdrop-saturate-150 text-black md:text-white md:bg-transparent py-4 px-6 md:p-0 rounded-bl-lg`} style={{ transition: 'opacity .15s ease-in' }}>
                 <div className="flex flex-col md:flex-row md:items-center gap-3 md:mr-4">
                     <Link onClick={() => {
+                        setShowMobileMenu(false)
                         // setLoading(true)
                     }} href="/" className='font-bold hover:underline border py-1 px-2'>Applications</Link>
                     <Link onClick={() => {
+                        setShowMobileMenu(false)
                         // setLoading(true)
                     }} href="/submittedapplication" className='font-bold hover:underline border py-1 px-2'>Submitted Application</Link>
                     <Link onClick={() => {
+                        setShowMobileMenu(false)
                         // setLoading(true)
                     }} href="/stats" className='font-bold hover:underline border py-1 px-2'>Stats</Link>
                     <Link onClick={() => {
+                        setShowMobileMenu(false)
                         // setLoading(true)
                     }} href="/funders" className='font-bold hover:underline border py-1 px-2'>Funders</Link>
                 </div>
                 {isAuthenticated ? <>
-                    <div className="mt-6 md:mt-0 text-sm font-bold cursor-pointer border border-slate-200 py-1 px-3" onClick={logout}>
+                    <div className="mt-6 md:mt-0 text-sm font-bold cursor-pointer border border-slate-200 py-1 px-3" onClick={() => { setShowMobileMenu(false); logout()}}>
                         Log out
                     </div>
                     <div className="mt-2 md:mt-0 flex flex-row-reverse items-center justify-end gap-3">
@@ -59,7 +63,7 @@ export default function Nav() {
                         <div className="lg:hidden cursor-pointer" title={user?.email}><FaUserCircle /></div>
                     </div>
                 </> :
-                    <Link href={'/login'} className='mt-6 md:mt-0 text-sm font-bold cursor-pointer'>Login</Link>
+                    <Link href={'/login'} onClick={() => { setShowMobileMenu(false) }} className='mt-6 md:mt-0 text-sm font-bold cursor-pointer'>Login</Link>
                 }
             </div>
         </header>}
